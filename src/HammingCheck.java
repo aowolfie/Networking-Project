3e0e7c8742e6b66a89de541a37c6ce72c0592d07
bluestar1996zz <bluestar1996zz@gmail.com>
2016-11-30 13:31:50 -0500

    Stripped out some components.

--- src/HammingCheck.java
+++ src/HammingCheck.java
@@ -9,47 +9,8 @@ class HammingCheck {
 	public static void main(String args[]) {
         String in = "1101001100110101";
         HammingEncode encode = new HammingEncode(in);
-        receive(nomString("011100110011001110101"),encode.getNumParityBits());
+        System.out.println(receive(nomString("011100110011001110101"),encode.getNumParityBits()));
     }
-
-	private static void miniMain(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the amount of bits you want to check:");
-        int n = scan.nextInt();
-        int a[] = new int[n];
-
-        for(int i=0 ; i < n ; i++) {
-            System.out.println("Enter the bit number: " + (n-i) + ":");
-            a[n-i-1] = scan.nextInt();
-        }
-
-        System.out.println("You entered:");
-        for(int i=0 ; i < n ; i++) {
-            System.out.print(a[n-i-1]);
-        }
-        System.out.println();
-
-        int b[] = generateCode(a);
-
-        System.out.println("The Generated Hamming code is:");
-        for(int i=0 ; i < b.length ; i++) {
-            System.out.print(b[b.length-i-1]);
-        }
-        System.out.println();
-
-        // Find the difference between the original and new array will give how many parity bits are needed
-        System.out.println("Enter the position of a bit to check for error detection:");
-        int error = scan.nextInt();
-        if(error != 0) {
-            b[error-1] = (b[error-1]+1)%2;
-        }
-        System.out.println("Input is:");
-        for(int i=0 ; i < b.length ; i++) {
-            System.out.print(b[b.length-i-1]);
-        }
-        System.out.println();
-        receive(b, b.length - a.length);
-	}
 
     private static int[] nomString(String input){
         int[] out = new int[input.length()];
@@ -58,76 +19,6 @@ class HammingCheck {
         }
         return out;
     }
-	
-	static int[] generateCode(int a[]) {
-		// Return the array 'b'.
-		int b[];
-		
-		// Find the number of parity bits required:
-		int i=0, parity_count=0 ,j=0, k=0;
-		while(i < a.length) {
-			// 2^(parity bits) = to current position
-			// Current position is (number of bits traveled + number of parity bits + 1).
-			// +1 is needed since array indices start from 0 whereas we need to start from 1.
-			
-			if(Math.pow(2,parity_count) == i+parity_count + 1) {
-				parity_count++;
-			}
-			else {
-				i++;
-			}
-		}
-		
-		// Length of 'b' is length of original data (a) + number of parity bits.
-		b = new int[a.length + parity_count];
-		
-		// Initialize this array with '2' to show an 'unset' value in the parity bit locations:
-		
-		for(i=1 ; i <= b.length ; i++) {
-			if(Math.pow(2, j) == i) {
-			// Found a parity bit location.
-			// Adjusting with (-1) to account for array indices starting from 0 instead of 1.
-				
-				b[i-1] = 2;
-				j++;
-			}
-			else {
-				b[k+j] = a[k++];
-			}
-		}
-		for(i=0 ; i < parity_count ; i++) {
-			// Setting even parity bits at parity bit locations:
-			
-			b[((int) Math.pow(2, i))-1] = getParity(b, i);
-		}
-		return b;
-	}
-	
-	static int getParity(int b[], int power) {
-		int parity = 0;
-		for(int i=0 ; i < b.length ; i++) {
-			if(b[i] != 2) {
-				// If 'i' doesn't contain an unset value,
-				// We will save that index value in k, increase it by 1,
-				// Then we convert it into binary:
-				
-				int k = i+1;
-				String s = Integer.toBinaryString(k);
-				
-				//If the bit at the 2^(power) location of the binary value of index is 1
-				//Then we need to check the value stored at that location.
-				//Check if that value is 1 or 0, we will calculate the parity value.
-				
-				int x = ((Integer.parseInt(s))/((int) Math.pow(10, power)))%10;
-				if(x == 1) {
-					if(b[i] == 1) {
-						parity = (parity+1)%2;
-					}
-				}
-			}
-		}
-		return parity;
-	}
 	
 	static String receive(int a[], int parity_count) {
 		// This is the receiver code. It receives a Hamming code in array 'a'.
